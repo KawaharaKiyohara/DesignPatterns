b928b1c6b9d7ac85fc3834ce7d8fbf7d88680f86
TakayukiKiyohara <nezumimusume30@gmail.com>
2017-05-19 00:16:53 +0900

    Adapterのサンプルプログラムを追加

---
+++ Adapter/Adapter/ModelDX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class ModelDX
+    {
+        public void Draw()
+        {
+            Console.WriteLine("DirectX Draw!!!");
+        }
+        public void BeginDraw()
+        {
+            Console.WriteLine("DirectX PreDraw!!!");
+        }
+        public void EndDraw()
+        {
+            Console.WriteLine("DirectX PostDraw!!!");
+        }
+    }
+}
---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            while (true)
+            {
+                Console.WriteLine("プラットフォームを選択してください。");
+                Console.WriteLine("0:DirectX、1:OpenGL、2:PS4");
+                int pl = int.Parse(Console.ReadLine());
+                //DirectX用のモデル描画処理のインスタンスを生成。
+                if (pl == 0)
+                {
+                    game.model = new ModelDX();
+                    break;
+                }
+                //そのほかのプラットフォームはどうしよう・・・
+                if (pl == 1)
+                {
+                    
+                }
+                if (pl == 2)
+                {
+
+                }
+                
+            }
+            game.Run();
+        }
+    }
+}
